645ea96411ed1ac0886c8ecf2ffbeed79f9755d9
Michael McClellan <mcclellan.m@gmail.com>
2018-12-05 12:27:09 -0500

    Add function docstrings

--- src/hello.py
+++ src/hello.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python
 
 def print_thing(string_to_print):
+	"""Print Function"""
 	new_thing = string_to_print + "hola!"
 
 	return new_thing
 
 def main():
+	"""Main Function"""
 	first_string = "hello world and "
 	new_string = print_thing(first_string)
 	print(new_string)
